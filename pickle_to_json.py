edc18c9a5ef7ae4c329db478178d7cd86d6fbebe
Brice <bjw@plu.edu>
2020-11-22 19:46:18 -0800

    testing with another input file

--- pickle_to_json.py
+++ pickle_to_json.py
@@ -4,21 +4,36 @@ import pickle as pickle
 import json
 import numpy as np
 
-def convert_dict_to_json(file_path):
-    with open(file_path, 'rb') as pickle, open('%s.json' % file_path, 'w') as fjson:
-        data = pickle.load(pickle)
-        for val in data:
-            for i,npval in enumerate(val):
-                print(type(npval))
-                if isinstance(npval,(np.ndarray)):
-                    val[i] = npval.tolist()
-                    print(type(val[i]))
+def convert_dict_to_json(file_path, data):
+    with open('%s.json' % file_path, 'w') as fjson:
+        print(data)
+        print(type(data))
+        for npval in data:
+            print(npval)
+            for keyval in data[npval]:
+                if isinstance(data[npval][keyval],(np.ndarray)):
+                    print("\n!ARRAY!\n")
+                    print(data[npval][keyval])
+                    data[npval][keyval] = data[npval][keyval].tolist()
         json.dump(data, fjson, ensure_ascii=False, sort_keys=True, indent=4)
 
 
 def main():
-    file_path = '/Users/brice/Desktop/Classes/CSCI387/Project/567ebca0f59c612eb977065008aad867/Test_subset.pkl'
-    convert_dict_to_json(file_path)
+    # pickle_path = '/Users/brice/Desktop/Classes/CSCI387/Project/Raw Datasets/Test_subset.pkl'
+    pickle_path = '/Users/brice/Desktop/Classes/CSCI387/Project/Raw Datasets/Project_1_Kyle/CASP10/T0705/Zhang-Server_TS1.pkl'
+    with open(pickle_path, 'rb') as pik:
+        file_paths = '/Users/brice/Desktop/Classes/CSCI387/Project/Raw Datasets/Zhang-Server_TS1_a'
+        data = pickle.load(pik)
+        convert_dict_to_json(file_paths, data)
+    
+    # pickle_path = '/Users/brice/Desktop/Classes/CSCI387/Project/Raw Datasets/Project_1_Kyle/CASP10/T0705/Zhang-Server_TS1.pkl'
+    # with open(pickle_path, 'rb') as pik:
+    #     file_paths = ['/Users/brice/Desktop/Classes/CSCI387/Project/Raw Datasets/Zhang-Server_TS1_a','/Users/brice/Desktop/Classes/CSCI387/Project/Raw Datasets/Zhang-Server_TS1_b']
+    #     data = pickle.load(pik)
+    #     for i in [0,1]:
+    #         val = data[i] 
+    #         print(type(val))
+    #         convert_dict_to_json(file_paths[i], val)
 
 if __name__ == '__main__':
     main()
